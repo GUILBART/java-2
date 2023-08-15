6aa67021ccb84132cd93b2a176c334be43de8f8c
Guilhereme maçanaro <guilhermevaldeir85@gmail.com>
2023-08-15 19:32:57 +0000

    Exemplo da aula

---
+++ exemplo.java
@@ -0,0 +1,11 @@
+public class exemplo {
+    public static void main(String[]args){
+        Aluno al = new Aluno();
+
+        al.setNome("Guilherme");
+        System.out.println(al.getNome());
+    
+        System.out.println("ola mundo");
+    }
+    
+}
